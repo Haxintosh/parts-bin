d852c2d32b9a552b558e4a27cd09a232a58a28cb
Haxintosh <goose@sillygoosy.ca>
2024-08-07 11:44:19 -0400

    basic api

--- main.py
+++ main.py
@@ -7,26 +7,57 @@ from openai import OpenAI
 
 import bom_parsers
 import utils
+
+from typing import Union
+from fastapi import FastAPI
+
 load_dotenv()
 
 MAIN_DB_URI= os.getenv("MONGODB_CONN_URI")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def get_component_db():
-    client = MongoClient(MAIN_DB_URI)
-    return client['components']
+fastapi_app = FastAPI()
+
+client = MongoClient(MAIN_DB_URI)
+db = client['components']
 
 def get_openai_client():
     client = OpenAI(api_key=OPENAI_API_KEY)
     return client
 
+@fastapi_app.get('/api/components/get_all')
+def get_all():
+    return utils.get_all_components(db)
+
+@fastapi_app.get('/api/components/{type}')
+def get_by_type(type:str):
+    return utils.get_components_by_type(db, type)
+
+@fastapi_app.get('/api/components/mpn/{mpn}')
+def get_by_mpn(mpn:str):
+    return utils.get_components_by_mpn(db, mpn)
+
+# @fastapi_app.post('/api/components/add')
+# def add_component(component: Union[bom_parsers.Component, dict]):
+#     if isinstance(component, dict):
+#         component = bom_parsers.Component(**component)
+#     return utils.add_component(db, component.type, component.dict())
+
+@fastapi_app.post('/api/components/add')
+def add_component(component: dict):
+    return utils.add_component(db, component['type'], component)
+
+@fastapi_app.post('/api/components/change_qty')
+def change_qty(type:str, mpn:str, qty:int):
+    return utils.change_component_qty(db, type, mpn, qty)
+
+@fastapi_app.post('/api/components/add_from_parsed_bom')
+def add_from_parsed_bom(parsed_bom: dict):
+    return utils.add_components_from_parsed_bom(db, parsed_bom)
+
+@fastapi_app.get('/api/components/find/{str}')
+def find(str:str):
+    return utils.db_find_from_str(db, str)
+
 if __name__ == '__main__':
     client = get_openai_client()
-    rows = bom_parsers.parse_csv("tests/LCSC.csv")
-    c, u = bom_parsers.lcsc_parser(rows)
-    bom_parsers.ai_parser_unsafe(u, client)
-    print(c['Other'][3])
-    # print(c['Other'][0])
-    # db = get_component_db()
-    # utils.add_components_from_parsed_bom(db, c)
-
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson import ObjectId
 
 def add_components_from_parsed_bom(db, dict):
         for k, v in dict.items():
@@ -15,24 +16,30 @@ def add_components_from_parsed_bom(db, dict):
                 else:
                     collection.insert_one(i)
 
+
 def get_all_components(db):
     return_list = {}
     for i in db.list_collection_names():
-        return_list[i] = list(db[i].find({}))
+        components = list(db[i].find({}))
+        return_list[i] = convert_objectid_to_str(components)
     return return_list
 
 def get_components_by_type(db, type):
-    return list(db[type].find({}))
+    return convert_objectid_to_str(list(db[type].find({})))
 
 def get_components_by_mpn(db, mpn):
-    return list(db.find({"mpn": mpn}))
+    for index, val in enumerate(db.list_collection_names()):
+        c = list(db[val].find({"mpn": mpn}))
+        if c:
+            convert_objectid_to_str(c)
+    return {"message": "Component not found"}
 
 def change_component_qty(db, type, mpn, qty):
     try:
         db[type].update_one({"mpn": mpn}, {"$set": {"qty": qty}})
     except:
-        return False
-    return True
+        return {"message": "Component not found"}
+    return {"message:": "Component updated"}
 
 def add_component(db, type, component):
     try:
@@ -40,3 +47,22 @@ def add_component(db, type, component):
         return True
     except:
         return False
+
+def convert_objectid_to_str(data):
+    if isinstance(data, list):
+        return [convert_objectid_to_str(item) for item in data]
+    elif isinstance(data, dict):
+        return {key: convert_objectid_to_str(value) for key, value in data.items()}
+    elif isinstance(data, ObjectId):
+        return str(data)
+    else:
+        return data
+
+
+def db_find_from_str(db, str):
+    return_array = []
+    for i in db.list_collection_names():
+        c = list(db[i].find({"$text": {"$search": str}}))
+        if c:
+            return_array.append(c)
+    return convert_objectid_to_str(return_array)
